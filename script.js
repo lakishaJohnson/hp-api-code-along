a41c002383d31019f4e16e6e69fd1f2a9e8b12a1
Lakisha J <lakishajohnson@pursuit.org>
2021-10-13 17:36:18 -0400

    Step 2

--- script.js
+++ script.js
@@ -10,7 +10,20 @@ fetch("http://hp-api.herokuapp.com/api/characters/house/gryffindor")
     console.log(characters);
     
     //STEP 2 <------------------------USE DATA TO ADD CHARACTER NAMES TO THE DROPDOWN MENU------------------------------->
-    
+    //Selects dropDown from HTML
+    let dropDown = document.querySelector("#dropDown");
+    //for loop to add each characters name to dropdown menu from data/js object 
+    for(let character of characters) {
+        //using DOM to create new tag <option> for each character
+        const option = document.createElement("option")
+        //adding a value to each option which is character name
+        option.setAttribute("value", character.name)
+        //adding content to be displayed which is character name
+        option.textContent = character.name
+        //we end up w/an option for each character name
+        //appends option to dropDown menu
+        dropDown.append(option);
+    }
     //STEP 3 <--------------------------------DISPLAY SELECTED CHARACTER'S INFO------------------------------------------->
     
     
